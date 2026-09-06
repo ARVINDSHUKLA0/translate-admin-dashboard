8466e8e464d6e2cb8f364a44028da059e7de1677
Arvind Shukla <shuklaarvind2521@gmail.com>
2026-09-06 17:54:33 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,7 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
 
     return (
         <header className="admin-navbar d-flex align-items-center justify-content-between">
-
-            {/* LEFT SIDE */}
             <div className="navbar-left d-flex align-items-center gap-2">
-
-                {/* DESKTOP MENU */}
                 <button
                     type="button"
                     className="navbar-menu-button navbar-menu-desktop border-0 bg-transparent"
@@ -28,43 +24,24 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
                         }`}
                     ></i>
                 </button>
-
-
-                {/* SEARCH */}
                 <div className="navbar-search d-flex align-items-center">
-
                     <i className="fa-solid fa-magnifying-glass navbar-search-icon"></i>
-
                     <input
                         type="text"
                         placeholder="Search users, jobs, transactions..."
                     />
-
                 </div>
 
             </div>
-
-
-            {/* RIGHT SIDE */}
             <div className="navbar-right d-flex align-items-center gap-3">
-
-                {/* NOTIFICATION */}
+                
                 <button
                     type="button"
                     className="navbar-notification d-flex align-items-center justify-content-center border-0"
                 >
                     <i className="fa-solid fa-bell"></i>
                 </button>
-
-
-                {/* ADMIN */}
-                <div className="navbar-avatar d-flex align-items-center justify-content-center">
-                    AD
-                </div>
-
-
-                {/* MOBILE MENU */}
-                <button
+                 <button
                     type="button"
                     className="navbar-menu-button navbar-menu-mobile border-0 bg-transparent"
                     onClick={toggleSidebar}
@@ -82,6 +59,11 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
                         }`}
                     ></i>
                 </button>
+                <div className="navbar-avatar d-flex align-items-center justify-content-center">
+                    AD
+                </div>
+ 
+               
 
             </div>
 
